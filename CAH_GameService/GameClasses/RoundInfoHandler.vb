3f2e8b7415f67132a8c935cc1cc0aad0518145af
adamatri <k.adam.atri@gmail.com>
2014-07-21 17:52:11 -0400

    fixed small logic error

--- CAH_GameService/GameClasses/RoundInfoHandler.vb
+++ CAH_GameService/GameClasses/RoundInfoHandler.vb
@@ -3,7 +3,7 @@ Public Class RoundInfoHandler
 
     Function GetRoundInfo(GameID As Int32) As RoundStuff
         Dim round As Int32 = ActiveGameTracker.GetRound(GameID)
-        If round >= GameStatus.ROUND1 Or round <= GameStatus.ROUND5 Then
+        If round >= GameStatus.ROUND1 And round <= GameStatus.ROUND5 Then
             Return CAH_Repository.RoundInfoByGameID(GameID, round - 20)
         Else
             Throw New InvalidRoundException(New CustomErrorDetail With { _
