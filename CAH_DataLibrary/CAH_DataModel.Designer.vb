e1155301c810c822c2a33bc542f1ab743f9e785a
adamatri <k.adam.atri@gmail.com>
2014-07-22 12:45:23 -0400

    Enhancement to DataModel layout

--- CAH_DataLibrary/CAH_DataModel.Designer.vb
+++ CAH_DataLibrary/CAH_DataModel.Designer.vb
@@ -1,4 +1,4 @@
-﻿' T4 code generation is enabled for model 'C:\Users\Developer\documents\visual studio 2013\Projects\CARDS_AGAINST_HUMANITY\CAH_DataLibrary\CAH_DataModel.edmx'. 
+﻿' T4 code generation is enabled for model 'C:\Users\Developer\Source\Repos\CARDS_AGAINST_HUMANITY\CAH_DataLibrary\CAH_DataModel.edmx'. 
 ' To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 ' property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 ' is open in the designer.
